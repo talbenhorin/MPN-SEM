c43390df1c1e621b2abc4fdbe40e9b3531c27252
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2021-09-20 15:59:46 -0400

    Update

--- MPNsem.R
+++ MPNsem.R
@@ -1,3 +1,19 @@
 rm(list=ls(all=TRUE))
-
+set.seed(1234)
 MPN <- read.csv("oysterMPN.csv", fill = FALSE, header = TRUE) 
+# install.packages("lavaan", dependencies = TRUE)
+# install.packages("semPlot", dependencies = TRUE)
+
+library(lavaan)
+
+oyster.df <- data.frame(chlo = MPN$chlo,temp = MPN$s.temp,sal = MPN$s.sal, water = MPN$s.tlh,oyster = MPN$log.tlh)
+
+model <- 'oyster ~ temp + sal + chlo + water
+          water ~ temp + sal + chlo
+'
+
+path.fit <- sem(model,
+                meanstructure=T,
+                data=oyster.df)
+summary(path.fit)
+semPlot::semPaths(path.fit,"reg",intercepts = FALSE)
