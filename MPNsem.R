b332fd710c728046e78d70c7ad04c73330ed1691
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2021-09-21 16:15:12 -0400

    Update

--- MPNsem.R
+++ MPNsem.R
@@ -7,8 +7,8 @@ MPN <- read.csv("oysterMPN.csv", fill = FALSE, header = TRUE)
 library(lavaan)
 
 oyster.df <- data.frame(chlo = MPN$chlo,temp = MPN$s.temp,
-                        sal = MPN$s.sal, water = MPN$water.log.tlh,
-                        oyster = MPN$log.tlh)
+                        sal = MPN$s.sal, water = MPN$water.log.pilf,
+                        oyster = MPN$log.pilf)
 
 model <- 'oyster ~ temp + sal + chlo + water
           water ~ temp + sal + chlo
@@ -18,4 +18,15 @@ path.fit <- sem(model,
                 meanstructure=T,
                 data=oyster.df)
 summary(path.fit)
-semPlot::semPaths(path.fit,"reg",intercepts = FALSE)
+semPlot::semPaths(path.fit,what = "std",
+                  whatLabels = "std",
+                  layout = "tree3",
+                  curvature = 2,
+                  intercepts = FALSE,
+                  nCharNodes = 0,
+                  edge.label.cex = 0.8,
+                  residuals = FALSE,
+                  pastel = TRUE,
+                  fade = TRUE,
+                  cardinal = FALSE,
+                  centerLevels = TRUE)
